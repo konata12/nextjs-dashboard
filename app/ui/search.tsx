cc1e0294eb113579c0dc7be0fc8fb95063edd3a1
dima <87605146+konata12@users.noreply.github.com>
2025-01-17 11:19:03 +0200

    lesson 16

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,7 +20,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300)
-  console.log(searchParams.get('query')?.toString())
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
